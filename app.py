5f9f9aff5638dbdd6606ab75d99ed24ec56aaf9a
amarisg25 <98620597+amarisg25@users.noreply.github.com>
2024-10-15 07:11:41 -0400

    gc auser input never

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ llm = ChatOpenAI(model_name=selected_model, temperature=0, openai_api_key=api_ke
 # Patient (Chatbot-user)
 patient = autogen.UserProxyAgent(
     name="patient",
-    human_input_mode="ALWAYS",
+    human_input_mode="NEVER",
     max_consecutive_auto_reply=10,
     code_execution_config={"work_dir": "coding", "use_docker": False},
     llm_config=llm_config
