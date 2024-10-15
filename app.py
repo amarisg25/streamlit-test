945cee957771bb6401df068447aea271026583b8
amarisg25 <98620597+amarisg25@users.noreply.github.com>
2024-10-14 22:44:20 -0400

    gc test

--- app.py
+++ app.py
@@ -339,7 +339,7 @@ with st.container():
 
         async def initiate_chat():
             await patient.a_initiate_chat(
-                FAQ_agent,
+                manager,
                 message=user_input,
             )
 
