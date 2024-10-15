86779f4b6ee6201b43d4c23270ec3ea3752a664e
amarisg25 <98620597+amarisg25@users.noreply.github.com>
2024-10-15 10:14:58 -0400

    sqlite

--- app.py
+++ app.py
@@ -14,11 +14,9 @@ from langchain import hub
 import autogen
 import chromadb
 __import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
-# CONFIGURATION
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
+__import__('pysqlite3') 
+import sys 
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3') 
 
 # Load environment variables
 load_dotenv()
